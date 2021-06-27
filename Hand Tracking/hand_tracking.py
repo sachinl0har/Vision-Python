ed4f186b8868b9542a63483d1ac3ee88fa69fb08
Sachin.B.Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-06-27 19:07:30 +0530

    first commit

---
+++ Hand Tracking/hand_tracking.py
@@ -0,0 +1,27 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                # print(id, cx, cy)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
